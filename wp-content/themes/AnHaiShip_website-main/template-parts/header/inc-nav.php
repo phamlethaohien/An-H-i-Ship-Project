70a135df9b4d60a55cce7a7280ff32ad24122a8e
Pham Le Thao Hien <phamlethaohien@github.com>
2022-04-14 19:04:23 +0700

    Header _#1

--- wp-content/themes/AnHaiShip_website-main/template-parts/header/inc-nav.php
+++ wp-content/themes/AnHaiShip_website-main/template-parts/header/inc-nav.php
@@ -5,72 +5,85 @@ $nav_top_sticky   =   $AnHaiShipWebsite_options['AnHaiShipWebsite_opt_nav_sticky
 $AnHaiShipWebsite_opt_logo_image_id    =   $AnHaiShipWebsite_options['AnHaiShipWebsite_opt_logo_image']['id'];
 ?>
 
-<nav id="site-navigation" class="main-navigation<?php echo esc_attr( $nav_top_sticky == 1 ? ' active-sticky-nav' : '' ); ?>">
-    <div class="site-navbar navbar-expand-lg">
-        <div class="container">
-            <div class="site-navigation_warp d-flex justify-content-lg-end">
-                <div class="site-logo d-flex align-items-center">
-                    <a href="<?php echo esc_url( get_home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>">
-                        <?php
-                            if ( !empty( $AnHaiShipWebsite_opt_logo_image_id ) ) :
-                                echo wp_get_attachment_image( $AnHaiShipWebsite_opt_logo_image_id, 'full' );
-                            else :
-                                echo'<img class="logo-default" src="'.esc_url( get_theme_file_uri( '/assets/images/logo.png' ) ).'" alt="'.get_bloginfo('title').'" />';
-                            endif;
-                        ?>
-                    </a>
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#site-menu" aria-controls="site-menu" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa fa-bars" aria-hidden="true"></i>
-                    </button>
+<!-- Top Header -->
+<div class="top_header d-md-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <p class="welcome" style="font-weight: bold;">
+                    Công ty TNHH Dịch vụ và Thương mại vận tải An Hải
+                </p>
+            </div>
+            <div class="col-6">
+                <div class="social">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-google" aria-hidden="true"></i></a>
                 </div>
-
-                <div id="site-menu" class="site-menu collapse navbar-collapse d-lg-flex justify-content-lg-end">
-
-                    <?php
-
-                    if ( has_nav_menu('primary') ) :
-
-                        wp_nav_menu( array(
-                            'theme_location' => 'primary',
-                            'menu_class'     => 'navbar-nav',
-                            'container'      => false,
-                        ) ) ;
-
-                    else:
-
-                    ?>
-
-						<ul class="main-menu navbar-nav ms-auto">
-							<li class="nav-item dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">About Us</a>
-								<div class="sub-menu dropdown-menu">
-									<a href="#" class="dropdown-item">Film</a>
-									<a href="#" class="dropdown-item">Team</a>
-								</div>
-							</li>
-							<!-- <li class="nav-item"><a class="nav-link" href="#about-us">About Us</a></li> -->
-							<li class="nav-item"><a class="nav-link" href="#businesses">Businesses</a></li>
-							<li class="nav-item"><a class="nav-link" href="#porfolio">Porfolio</a></li>
-							<li class="nav-item"><a class="nav-link" href="#news">News</a></li>
-							<li class="nav-item"><a class="nav-link" href="#english">English</a></li>
-							<?php
-							// TODO: If admin
-							if ( has_nav_menu('primary') ) :
-
-							?>
-								<li>
-									<a href="<?php echo get_admin_url().'/nav-menus.php'; ?>">
-										<?php esc_html_e( 'ADD TO MENU','AnHaiShipWebsite' ); ?>
-									</a>
-								</li>
-							<?php endif; ?>
-                        </ul>
-
-                    <?php endif; ?>
-
+                <div class="language">
+                    <a>Ngôn ngữ:</a>
+                    <a href="#">
+                        <img width="26" height="20" src="" alt="">
+                    </a>
+                    <a href="#">
+                        <img width="26" height="20" src="" alt="">
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-</nav>
+</div>
+
+<!-- Header - Menu -->
+<div class="header d-none d-md-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-2 logo">
+                <a href="#">
+                    <img src="https://anhaishipping.com.vn/wp-content/uploads/2021/09/logoAn-Hai.png" alt="">
+                </a>
+            </div>
+            <div class="col-10">
+                <div class="tag-header">
+                    <span>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        Số 110, Quận Hải An, Hải Phòng
+                    </span>
+                    <span>
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        chartering@anhaishipping.com.vn
+                    </span>
+                    <span>
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        0225 8830218
+                    </span>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="#">Trang chủ</a></li>
+                        <li>
+                            <a href="#">
+                                Về chúng tôi
+                                <!-- <i class="fa fa-angle-down" aria-hidden="true"></i> -->
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Dịch vụ
+                                <!-- <i class="fa fa-angle-down" aria-hidden="true"></i> -->
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Đội tàu
+                                <!-- <i class="fa fa-angle-down" aria-hidden="true"></i> -->
+                            </a>
+                        </li>
+                        <li><a href="#">Tin tức</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
