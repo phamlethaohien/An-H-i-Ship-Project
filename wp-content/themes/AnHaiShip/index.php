d444a61ed71aeebb4f14f87638918ca61d05dab0
CW-HoanPL <CW-HoanPL@github.com>
2022-04-27 12:28:27 +0700

    Archive page

--- wp-content/themes/AnHaiShip/index.php
+++ wp-content/themes/AnHaiShip/index.php
@@ -1,18 +1,24 @@
 <?php get_header(); ?>
 
 <main id="main">
-  <?php
-  if( have_posts() ) {
-    while( have_posts() ) {
-      the_post();
-      get_template_part( 'template-parts/archive/content', 'archive' );
-    }
-  }
-  ?>
+  <section id="archive" class="archive light">
+    <div class="container py-2">
+      <?php
+      if( have_posts() ) {
+        while( have_posts() ) {
+          the_post();
+          get_template_part( 'template-parts/archive/content', 'archive' );
+        }
+      }
+      ?>
+      <div class="text-center">
+        <?php
+          the_posts_pagination();
+        ?>
+      </div>
+    </div>
+  </section>
 
-  <?php
-    the_posts_pagination();
-  ?>
 </main>
 
 <?php get_footer(); ?>
--- wp-content/themes/AnHaiShip/template-parts/archive/content-archive.php
+++ wp-content/themes/AnHaiShip/template-parts/archive/content-archive.php
@@ -1,8 +1,28 @@
-<section id="archive" class="archive">
-    <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="">    
-    <h3><?php the_title(); ?></h3>
-    <div><?php the_date(); ?></div>
-    <div><?php comments_number(); ?></div>
-    <p><?php the_excerpt(); ?></p>
-    <a href="<?php the_permalink(); ?>">Read more</a>
-</section>
+
+<?php
+	$colors = array('blue', 'green', 'yellow', 'red');
+	$color = $colors[rand(0, count($colors) - 1)];
+?>
+
+<article class="postcard light <?php echo $color; ?>">
+	<div class=""><?php $color; ?></div>
+	<a class="postcard__img_link" href="<?php the_permalink(); ?>">
+		<img class="postcard__img" src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="<?php the_title(); ?>" />
+	</a>
+	<div class="postcard__text t-dark">
+		<h1 class="postcard__title <?php echo $color; ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<div class="postcard__subtitle small">
+			<time datetime="2022-04-25 12:00:00">
+				<i class="bi bi-calendar-date"></i> <?php the_date(); ?>
+			</time>
+		</div>
+		<div class="postcard__bar"></div>
+		<div class="postcard__preview-txt"><?php the_excerpt(); ?></div>
+		<ul class="postcard__tagbox">
+			<li class="tag__item"><i class="bi bi-chat-dots-fill"></i> <?php comments_number(); ?></li>
+			<li class="tag__item play <?php echo $color; ?>">
+				<a href="<?php the_permalink(); ?>"><i class="bi bi-play-btn-fill"></i> Read more</a>
+			</li>
+		</ul>
+	</div>
+</article>
