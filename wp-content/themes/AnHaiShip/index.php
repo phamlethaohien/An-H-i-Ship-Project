6a9f9bbd367947f3ec762a22044987ebd435065d
CW-HoanPL <CW-HoanPL@github.com>
2022-04-28 13:07:04 +0700

    Limited feature

--- wp-content/themes/AnHaiShip/index.php
+++ wp-content/themes/AnHaiShip/index.php
@@ -1,24 +1,18 @@
 <?php get_header(); ?>
 
 <main id="main">
-  <section id="archive" class="archive light">
-    <div class="container py-2">
-      <?php
-      if( have_posts() ) {
-        while( have_posts() ) {
-          the_post();
-          get_template_part( 'template-parts/archive/content', 'archive' );
-        }
-      }
-      ?>
-      <div class="text-center">
-        <?php
-          the_posts_pagination();
-        ?>
-      </div>
-    </div>
-  </section>
+  <?php
+  if( have_posts() ) {
+    while( have_posts() ) {
+      the_post();
+      get_template_part( 'template-parts/archive/content', 'archive' );
+    }
+  }
+  ?>
 
+  <?php
+    the_posts_pagination();
+  ?>
 </main>
 
 <?php get_footer(); ?>
--- wp-content/themes/AnHaiShip/search.php
+++ wp-content/themes/AnHaiShip/search.php
@@ -1,24 +1,14 @@
 <?php get_header(); ?>
 
 <main id="main">
-  <section id="archive" class="archive light">
-    <div class="container py-2">
-      <?php
-      if( have_posts() ) {
-        while( have_posts() ) {
-          the_post();
-          get_template_part( 'template-parts/archive/content', 'archive' );
-        }
-      }
-      ?>
-      <div class="text-center">
-        <?php
-          the_posts_pagination();
-        ?>
-      </div>
-    </div>
-  </section>
-
+  <?php
+  if( have_posts() ) {
+    while( have_posts() ) {
+      the_post();
+      get_template_part( 'template-parts/archive/content', 'archive' );
+    }
+  }
+  ?>
 </main>
 
 <?php get_footer(); ?>
